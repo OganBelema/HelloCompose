ea405e9b864f96d52ef8eec1003cf906c2248bee
Oganbelema <oganbelema@gmail.com>
2022-08-16 10:53:53 +0100

    added profile image

--- app/src/main/java/com/oganbelema/hellocompose/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocompose/MainActivity.kt
@@ -3,7 +3,10 @@ package com.oganbelema.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,6 +15,7 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.oganbelema.hellocompose.ui.theme.HelloComposeTheme
@@ -38,11 +42,34 @@ fun CreateBusinessCard() {
     Surface( modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp).height(400.dp).padding(16.dp),
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(400.dp)
+            .padding(16.dp),
             shape = RoundedCornerShape(corner = CornerSize(16.dp)),
             elevation = 8.dp) {
-            
+
+            CreateProfileImage()
         }
+    }
+}
+
+@Composable
+private fun CreateProfileImage() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(6.dp),
+        shape = CircleShape,
+        border = BorderStroke(1.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.user),
+            contentDescription = "User image",
+            modifier = Modifier.size(120.dp)
+        )
     }
 }
 
