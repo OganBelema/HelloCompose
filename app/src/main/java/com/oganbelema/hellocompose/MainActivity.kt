2f48839e6beef247a71a4d67bff4e47b65123e3c
Oganbelema <oganbelema@gmail.com>
2022-08-16 10:34:52 +0100

    added card background to the ui

--- app/src/main/java/com/oganbelema/hellocompose/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocompose/MainActivity.kt
@@ -3,15 +3,17 @@ package com.oganbelema.hellocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.oganbelema.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +35,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBusinessCard() {
-    Surface( modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
-
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp).height(400.dp).padding(16.dp),
+            shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+            elevation = 8.dp) {
+            
+        }
     }
 }
 
